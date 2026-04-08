ac9030804575c2dca317f2bf62184e7d90b8dc23
abhishek chaudhary <abhishek0221997@gmail.com>
2026-04-08 11:21:40 +0530

    updated

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -29,9 +29,9 @@ const Connections = () => {
     <div className="text-center my-10">
       <h2 className="text-bold text-amber-600 text-2xl">Connections</h2>
       {connections.map((connection) => {
-        const { firstName, lastName, photoUrl, age, gender, bio } = connection;
+        const {_id,firstName, lastName, photoUrl, age, gender, bio } = connection;
         return (
-          <div className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto">
+          <div key={_id} className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto">
             <div>
               <img alt="photo" className="w-20  rounded-full"  src={photoUrl} />
             </div>
