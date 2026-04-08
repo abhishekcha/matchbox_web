b7a6f3f9efb14a1922fc1cf91e04211b8eb65a79
abhishek chaudhary <abhishek0221997@gmail.com>
2026-04-08 11:22:42 +0530

    added Request.jsx for fetching how many request got

---
+++ src/components/Requests.jsx
@@ -0,0 +1,57 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { BASE_URl } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addRequests } from "../utils/requestSlice";
+
+const Requests = () => {
+  const requests = useSelector((store) => store.requests);
+  const dispatch = useDispatch();
+  const fetchRequests = async () => {
+    try {
+      const res = await axios.get(BASE_URl + "/user/requests/received", {
+        withCredentials: true,
+      });
+      dispatch(addRequests(res.data.data));
+    } catch (error) {}
+  };
+  useEffect(() => {
+    fetchRequests();
+  }, []);
+  if (!requests) return;
+  if (requests.length === 0) {
+    return <h1>No Requests Found</h1>;
+  }
+  return (
+    <div className="text-center my-10">
+      <h2 className="text-bold text-amber-600 text-2xl">Connections Request</h2>
+      {requests.map((request) => {
+        const { _id, firstName, lastName, photoUrl, age, gender, bio } =
+          request.fromUserId;
+        return (
+          <div
+            key={_id}
+            className="flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 w-2/3 mx-auto"
+          >
+            <div>
+              <img alt="photo" className="w-20  rounded-full" src={photoUrl} />
+            </div>
+            <div className="text-left mx-4">
+              <h2 className="font-bold text-xl">
+                {firstName + " " + lastName}
+              </h2>
+              {age && gender && <p>{age + "," + gender}</p>}
+              <p>{bio}</p>
+            </div>
+            <div className="flex">
+              <button className="btn btn-primary mx-2">Reject</button>
+              <button className="btn btn-secondary mx-2">Accept</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Requests;
