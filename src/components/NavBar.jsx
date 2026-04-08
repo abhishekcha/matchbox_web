d6419b229f43e5c7fb9d49ecb1279135efd5f435
abhishek chaudhary <abhishek0221997@gmail.com>
2026-04-08 23:06:37 +0530

    updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,72 +1,146 @@
-import React from "react";
-import { useSelector,useDispatch} from "react-redux";
+// import React from "react";
+// import { useSelector,useDispatch} from "react-redux";
+// import { Link, useNavigate } from "react-router";
+// import { BASE_URl } from "../utils/constants";
+// import axios from "axios";
+// import { removeUser } from "../utils/userSlice";
+
+// const NavBar = () => {
+//   const dispatch=useDispatch();
+//   const user = useSelector((store) => store.user);
+//   const navigate=useNavigate();
+//   //console.log(user);
+//   const handleLogout = async () => {
+//     try {
+//       await axios.post(
+//         BASE_URl + "/logout",
+//         {},
+//         { withCredentials: true },
+//       );
+//       dispatch(removeUser());
+//       return navigate("/login")
+//     } catch (error) {
+
+//     }
+//   };
+//   return (
+//     <div className="navbar bg-base-300">
+//       <div className="flex-1">
+//         <Link to="/" className="btn btn-ghost text-xl">
+//           👩‍💻MatchBox
+//         </Link>
+//       </div>
+//       {user && (
+//         <div className="dropdown dropdown-end mx-5 flex items-center">
+//           <p>welocme {user.firstName}</p>
+//           <div
+//             tabIndex={0}
+//             role="button"
+//             className="btn btn-ghost btn-circle avatar"
+//           >
+//             <div className="w-10 rounded-full">
+//               <img alt="user photo" src={user.photoUrl} />
+//             </div>
+//           </div>
+//           <ul
+//             tabIndex={0}
+//             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+//           >
+//             <li>
+//               <Link to="/profile" className="justify-between">
+//                 Profile
+//                 <span className="badge">New</span>
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="/connections">Connections</Link>
+//             </li>
+//              <li>
+//               <Link to="/requests">Request</Link>
+//             </li>
+//             <li>
+//               <Link onClick={handleLogout}>Logout</Link>
+              
+//             </li>
+//           </ul>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default NavBar;
+
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router";
 import { BASE_URl } from "../utils/constants";
-import axios from "axios";
 import { removeUser } from "../utils/userSlice";
 
 const NavBar = () => {
-  const dispatch=useDispatch();
   const user = useSelector((store) => store.user);
-  const navigate=useNavigate();
-  //console.log(user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const handleLogout = async () => {
     try {
-      await axios.post(
-        BASE_URl + "/logout",
-        {},
-        { withCredentials: true },
-      );
+      await axios.post(BASE_URl + "/logout", {}, { withCredentials: true });
       dispatch(removeUser());
-      return navigate("/login")
-    } catch (error) {
-
+      return navigate("/login");
+    } catch (err) {
+      // Error logic maybe redirect to error page
+      console.log(err);
     }
   };
+
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
         <Link to="/" className="btn btn-ghost text-xl">
-          👩‍💻MatchBox
+          👩‍💻 MatchBox
         </Link>
       </div>
       {user && (
-        <div className="dropdown dropdown-end mx-5 flex items-center">
-          <p>welocme {user.firstName}</p>
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img alt="user photo" src={user.photoUrl} />
+        <div className="flex-none gap-2">
+          <div className="form-control">Welcome, {user.firstName}</div>
+          <div className="dropdown dropdown-end mx-5 flex">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img alt="user photo" src={user.photoUrl} />
+              </div>
             </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link to="/profile" className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/connections">Connections</Link>
+              </li>
+
+              <li>
+                <Link to="/requests">Requests</Link>
+              </li>
+              <li>
+                <Link>Premium</Link>
+              </li>
+              <li>
+                <a onClick={handleLogout}>Logout</a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <Link to="/profile" className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/connections">Connections</Link>
-            </li>
-             <li>
-              <Link to="/requests">Request</Link>
-            </li>
-            <li>
-              <Link onClick={handleLogout}>Logout</Link>
-              
-            </li>
-          </ul>
         </div>
       )}
     </div>
   );
 };
-
 export default NavBar;
